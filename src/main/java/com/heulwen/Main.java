938ff08ef19b1f23370fe4119b0cbfe6afd91968
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2025-12-23 16:18:26 +0700

    exception

--- src/main/java/com/heulwen/Main.java
+++ src/main/java/com/heulwen/Main.java
@@ -1,17 +1,16 @@
 package com.heulwen;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /* RUNTIME EXCEPTIONS
+        * ArithmeticException -> 11/0
+        * ArrayIndexOutOfBoundsException -> int[] a = new int[2]; a[3] = 1
+        * ClassCastException -> Integer integer = (String) obj
+        * IllegalArgumentException -> Programmer - tham so truyen khong hop le // func(int a); double b; func(b)
+        * NullPointerException -> name.length() // name = null
+        * NumberFormatException -> Programmer - parse chuoi khong phai so // String a = "abc"; a = b.parseInt(a)
+        */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
